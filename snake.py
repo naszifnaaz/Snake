e7b502591052d4b6200db87141735da28fe3def8
naszifnaaz <naszifnaaz@gmail.com>
2020-11-04 13:31:38 +0530

    program template

--- snake.py
+++ snake.py
@@ -1,1 +1,52 @@
 #Classic Snake game using python and pygame
+
+import pygame
+import math
+import random
+from tkinter import messagebox
+
+#Cube Class
+class cube(object):
+    rows = 0
+    width = 0
+
+    def __init__(self, start, dirnx = 1, dirny = 0, color = (255,0,0)):
+        pass
+    
+    def move(self, dirnx, dirny):
+        pass
+    
+    def draw(Self, surface, eyes = False):
+        pass
+
+
+#Snake class
+class snake(object):
+
+    def __init__(self, color, pos):
+        pass
+    
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+    
+    def add_cube(self):
+        pass
+    
+    def draw(self, surface):
+        pass
+
+def draw_grid(width, rows, surface):
+    pass
+
+def snack(rows, items):
+    pass
+
+def message_box(subject, content):
+    pass
+
+def main():
+    pass
+
